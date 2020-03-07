bb07523b86a5b4b7bc6c8a144cef8a35dc32928b
bisov <a.bisov@naicoms.net>
2020-03-07 16:59:09 +0200

    new menu
    - Menu

---
+++ Assets/Ghoul_Menu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ghoul_Menu : MonoBehaviour
+{
+    Vector2 currentPos;
+    float speed = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < 20)
+        {
+            transform.Translate(Vector2.up * speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.position = currentPos;
+        }
+       
+        //transform.position = currentPos;
+        
+
+    }
+}
